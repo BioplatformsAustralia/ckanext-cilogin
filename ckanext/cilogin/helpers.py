10fc437ec21e85f76bec7966423bd659a0ac4925
Konstantin Sivakov <konstantin.sivakov@gmail.com>
2023-03-30 22:23:34 +0200

    Rename cilogon to cilogin

--- ckanext/cilogin/helpers.py
+++ ckanext/cilogin/helpers.py
@@ -8,11 +8,11 @@ from ckan.lib.api_token import decode, encode
 
 log = logging.getLogger(__name__)
 
-base_url = tk.config.get("ckanext.cilogon.base_url")
-username = tk.config.get("ckanext.cilogon.username")
-password = tk.config.get("ckanext.cilogon.password")
-coid = tk.config.get("ckanext.cilogon.coid")
-password_authenticator = tk.config.get("ckanext.cilogon.password_authenticator", "5")
+base_url = tk.config.get("ckanext.cilogin.base_url")
+username = tk.config.get("ckanext.cilogin.username")
+password = tk.config.get("ckanext.cilogin.password")
+coid = tk.config.get("ckanext.cilogin.coid")
+password_authenticator = tk.config.get("ckanext.cilogin.password_authenticator", "5")
 
 
 ci_login_dict = {
@@ -37,22 +37,22 @@ ci_login_dict = {
 
 def process_token(user, token):
     # get the user from clilogon
-    base_url = tk.config.get("ckanext.cilogon.base_url")
-    cilogon_user = _get_user(base_url, user)
-    if not cilogon_user:
-        log.error("Could not get user from cilogon")
+    base_url = tk.config.get("ckanext.cilogin.base_url")
+    cilogin_user = _get_user(base_url, user)
+    if not cilogin_user:
+        log.error("Could not get user from cilogin")
         return
-    cilogon_user_id = cilogon_user.get("CoPeople")[0].get("Id")
-    # get the token from cilogon
-    cilogon_token_dict = _get_token(base_url, cilogon_user_id, token)
-    if not cilogon_token_dict:
-        log.error("Could not get token from cilogon")
+    cilogin_user_id = cilogin_user.get("CoPeople")[0].get("Id")
+    # get the token from cilogin
+    cilogin_token_dict = _get_token(base_url, cilogin_user_id, token)
+    if not cilogin_token_dict:
+        log.error("Could not get token from cilogin")
         return
     # check if the token matches
-    cilogon_token = cilogon_token_dict.get("Passwords")[0].get("Password")
-    if not _compare_tokens(cilogon_token, token):
+    cilogin_token = cilogin_token_dict.get("Passwords")[0].get("Password")
+    if not _compare_tokens(cilogin_token, token):
         log.error("Token does not match")
-        _update_or_create_token(base_url, cilogon_user_id, cilogon_token_dict, token)
+        _update_or_create_token(base_url, cilogin_user_id, cilogin_token_dict, token)
         return
 
 
@@ -66,7 +66,7 @@ def _get_user(base_url,user):
     try:
         response = requests.get(url=user_url, auth=(username, password), params=params)
     except Exception as e:
-        log.error("Could not get user from cilogon")
+        log.error("Could not get user from cilogin")
         log.error(e)
     if response.status_code == 200:
         return json.loads(response.text)
@@ -74,37 +74,37 @@ def _get_user(base_url,user):
         log.error(response.reason)
 
 
-def _get_token(base_url, cilogon_user_id, token):
+def _get_token(base_url, cilogin_user_id, token):
     token_url = f"{base_url}/password_authenticator/passwords.json"
-    params = {"coid": coid, "copersonid": cilogon_user_id }
+    params = {"coid": coid, "copersonid": cilogin_user_id }
     try:
         response = requests.get(url=token_url, auth=(username, password), params=params)
     except Exception as e:
-        log.error("Could not get token from cilogon")
+        log.error("Could not get token from cilogin")
         log.error(e)
     if response.status_code == 200:
         return json.loads(response.text)
     elif response.status_code == 204:
-        log.info("No token found for user %s. We should add one", cilogon_user_id)
+        log.info("No token found for user %s. We should add one", cilogin_user_id)
         return _update_or_create_token(
-            base_url, cilogon_user_id, ci_login_dict, token
+            base_url, cilogin_user_id, ci_login_dict, token
         )
     else:
         log.error(response.reason)
 
 
-def _update_or_create_token(base_url, cilogon_user_id, cilogon_token_dict, token):
+def _update_or_create_token(base_url, cilogin_user_id, cilogin_token_dict, token):
     response = None
     token = six.ensure_str(encode(token)) if isinstance(token, dict) else six.ensure_str(token)
     token_url = f"{base_url}/password_authenticator/passwords.json"
-    cilogon_token_dict.get("Passwords")[0]["Password"] = token
-    cilogon_token_dict.get("Passwords")[0]["Person"]["Id"] = cilogon_user_id
-    cilogon_token_dict.get("Passwords")[0]["PasswordAuthenticatorId"] = password_authenticator
-    data = json.dumps(cilogon_token_dict)
+    cilogin_token_dict.get("Passwords")[0]["Password"] = token
+    cilogin_token_dict.get("Passwords")[0]["Person"]["Id"] = cilogin_user_id
+    cilogin_token_dict.get("Passwords")[0]["PasswordAuthenticatorId"] = password_authenticator
+    data = json.dumps(cilogin_token_dict)
     try:
         response = requests.post(url=token_url, auth=(username, password), data=data)
     except Exception as e:
-        log.error("Could not update token from cilogon")
+        log.error("Could not update token from cilogin")
         log.error(e)
     if response.status_code == 200:
         return json.loads(response.text)
@@ -112,8 +112,8 @@ def _update_or_create_token(base_url, cilogon_user_id, cilogon_token_dict, token
         log.error(response.reason)
 
 
-def _compare_tokens(cilogon_token, token):
+def _compare_tokens(cilogin_token, token):
     jti = None
     if isinstance(token, dict):
         jti = encode(token)
-    return cilogon_token == token or cilogon_token == jti
+    return cilogin_token == token or cilogin_token == jti
